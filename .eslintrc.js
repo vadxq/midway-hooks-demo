4252181f771bf663c1bb56a63be214225bf77e50
vadxq <dongxianlin@vadxq.com>
2021-03-24 16:48:48 +0800

    feat(prettier): update prettier

--- .eslintrc.js
+++ .eslintrc.js
@@ -15,12 +15,13 @@ module.exports = {
   extends: [
     'plugin:react/recommended',
     'plugin:@typescript-eslint/recommended', // Uses the recommended rules from the @typescript-eslint/eslint-plugin
-    'prettier/@typescript-eslint', // Uses eslint-config-prettier to disable ESLint rules from @typescript-eslint/eslint-plugin that would conflict with prettier
+    'prettier',
     'plugin:prettier/recommended', // Enables eslint-plugin-prettier and eslint-config-prettier. This will display prettier errors as ESLint errors. Make sure this is always the last configuration in the extends array.
   ],
   rules: {
     "no-console": process.env.NODE_ENV === "production" ? "warn" : "off",
     "no-debugger": process.env.NODE_ENV === "production" ? "warn" : "off",
+    'no-useless-escape': 2,
   },
   env: {
     browser: true,
